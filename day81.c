9b42c9edb0945e4d2099fb056d459821a504735e
Anvesha sharan <anvesha0606@gmail.com>
2026-04-23 23:49:55 +0530

    100daysof(DSA)day80

---
+++ day81.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[100];
+
+    /* Input array */
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    /* Bubble Sort */
+    for (int i = 0; i < n - 1; i++) {
+
+        int swapped = 0;  // optimization
+
+        for (int j = 0; j < n - i - 1; j++) {
+
+            /* Compare adjacent elements */
+            if (arr[j] > arr[j + 1]) {
+
+                /* Swap */
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                swapped = 1;
+            }
+        }
+
+        /* If no swap happened → already sorted */
+        if (swapped == 0)
+            break;
+    }
+
+    /* Print sorted array */
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
